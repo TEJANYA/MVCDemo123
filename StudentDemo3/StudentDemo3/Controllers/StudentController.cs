c4a2896c5fed3330742b94190717cc7eef7dd15a
TEJANYA <tejanya.1762@gmail.com>
2018-04-10 22:30:01 +0530

    Initial Commit

---
+++ StudentDemo3/StudentDemo3/Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentDemo3.Models;
+
+namespace StudentDemo3.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            Student stu = new Student();
+            stu.id = 1;
+            stu.name = "Teja";
+            stu.phonenum = 12345;
+            return View(stu);
+        }
+    }
+}
---
+++ StudentDemo3/StudentDemo3/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentDemo3.Models
+{
+    public class Student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int phonenum { get; set; }
+    }
+}
