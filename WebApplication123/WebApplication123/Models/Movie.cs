f472e1ac9104336debe18ce5412c892e78875168
TEJANYA <tejanya.1762@gmail.com>
2018-04-11 19:51:26 +0530

    just created for CRUD operations

---
+++ WebApplication123/WebApplication123/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication123.Models
+{
+    public class Movie
+    {
+        public int id;
+        public string title;
+        public int rating;
+    }
+}
